313011b37a5fc29a0bd67f9f3ac31bc77626211c
vvinacode <devinaa9327@gmail.com>
2025-04-27 21:30:11 +0700

    cek

--- index.php
+++ index.php
@@ -43,27 +43,6 @@
   </div>
 
   <!-- Menu -->
-  <?php
-
-  $page = isset($_GET['page']) ? $_GET['page'] : "";
-  $action = isset($_GET['action']) ? $_GET['action'] : "";
-
-  if ($page == "") {
-    include "NAMA_HALAMAN";
-  } elseif ($page == "NAMA_PAGE") {
-    if ($action == "") {
-      include "NAMA_HALAMAN";
-    } elseif ($action == "NAMA_ACTION") {
-      include "NAMA_HALAMAN";
-    } elseif ($action == "NAMA_ACTION") {
-      include "NAMA_HALAMAN";
-    } else {
-      include "NAMA_HALAMAN";
-    }
-  } else {
-    include "NAMA_HALAMAN";
-  }
-  ?>
 
   <!-- jquery -->
   <link rel="stylesheet" href="assets/js/jquery-3.7.0.min.js">
--- welcome.php
+++ welcome.php
@@ -1,1 +1,14 @@
-<h1>SELAMAT DATANG</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1 class="text-center">SELAMAT DATANG</h1>
+</body>
+
+</html>
